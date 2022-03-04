c47c60508f444ca642121084bc98abd3e7b7a31b
artiixes <dplosinski97@gmail.com>
2022-03-04 18:35:57 +0100

    innit project

---
+++ learning-git-task.py
@@ -0,0 +1,11 @@
+sklep_dict = {
+    "piekarnia": ["chleb", "paczek", "bulka"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+x = 0
+print("Lista zakupow")
+for i in sklep_dict:
+    a = sklep_dict.get(i)
+    x += len(a)
+    print("Ide do " + i.title() + " kupuje tu nastepujace rzeczy: " + str(a).title())
+print("W sumie kupuje " + str(x) + " produktow.")
